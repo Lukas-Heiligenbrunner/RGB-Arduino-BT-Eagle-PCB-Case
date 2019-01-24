ae0794bc78fdbac2c06762aef9869ebd5af1c65d
Lukas-Heiligenbrunner <30468956+Lukas-Heiligenbrunner@users.noreply.github.com>
2019-01-24 19:51:13 +0100

    used code form @TaviTav

--- Arduino_sketch/RGB_strip_v1.1.ino
+++ Arduino_sketch/RGB_strip_v1.1.ino
@@ -1,96 +1,98 @@
+/* 
+ *  compatible android app https://github.com/Lukas-Heiligenbrunner/RGB-Arduino-BT-App
+ *  
+ *  Make sure that :
+ *  
+ *  You disconnect rx & tx wires when you load the program to arduino
+ *  and...
+ *  BT rx --> Arduino tx
+ *  BT tx --> Arduino rx
+ *  Have fun!
+*/
+
+
 #include <SoftwareSerial.h>
+SoftwareSerial BLU(0,1);
 
-#define PINRED 9
-#define PINGREEN 10
-#define PINBLUE 11
+int redLed = 9;           // the Red LED
+int greenLed = 5;         // the green LED
+int blueLed = 6;          // the blue LED
 
-SoftwareSerial mySerial(A1, A0); // RX, TX
+int Red = 0;
+int Green = 0;
+int Blue = 0;
+
+int var;
+int startstring;
+int endstring;
+
+String BT="";
+String Red_string;
+String Green_string;
+String Blue_string;
 
 void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(57600);
-
-
-  Serial.println("Hello, programm starting up!");
-
-  //initializing softwareserial...
-  mySerial.begin(9600);
-
-  pinMode(PINRED,OUTPUT);
-  pinMode(PINGREEN,OUTPUT);
-  pinMode(PINBLUE,OUTPUT);
+    pinMode(redLed, OUTPUT);
+    pinMode(greenLed, OUTPUT);
+    pinMode(blueLed, OUTPUT);
+    BLU.begin(9600);
 }
 
-String result;
-String tempcolor;
-int red;
-int green;
-int blue;
-
-char temp;
-int i=1;
-
-void loop() {
-  if (mySerial.available()) {
-    temp = mySerial.read();
-    //result.remove(11);
-    if(temp == ',')
-    {
-      blue=hexToDec(tempcolor);
-      tempcolor="";
-      Serial.println(result);
-      result="";
-      i=1;
-      changecolor(red,green,blue);
-    }else
-    {
-      if(temp == '/')
-      {
-        if(i==1)
-        {
-          red =hexToDec(tempcolor);
-        }else if(i==2)
-        {
-          green=hexToDec(tempcolor);
-        }
-        Serial.println(i);
-        
-         i++;
-         tempcolor="";
-      }else 
-      {
-        tempcolor+=temp;
-      }
-      result+=temp;
+void loop(){
+    if (BLU.available()>0) {
+        translateBT();
     }
-}
+
 }
 
-void changecolor(int r, int g, int b)
-{
-  analogWrite(PINGREEN,r);
-  analogWrite(PINRED,g);
-  analogWrite(PINBLUE,b);
-  
+// Function for setting RGB value
+void setrgb() {
+    analogWrite(redLed, Red);         // R
+    analogWrite(greenLed, Green);     // G
+    analogWrite(blueLed, Blue);       // B
 }
 
-//by benrugg
-unsigned int hexToDec(String hexString) {
-  
-  unsigned int decValue = 0;
-  int nextInt;
-  
-  for (int i = 0; i < hexString.length(); i++) {
-    
-    nextInt = int(hexString.charAt(i));
-    if (nextInt >= 48 && nextInt <= 57) nextInt = map(nextInt, 48, 57, 0, 9);
-    if (nextInt >= 65 && nextInt <= 70) nextInt = map(nextInt, 65, 70, 10, 15);
-    if (nextInt >= 97 && nextInt <= 102) nextInt = map(nextInt, 97, 102, 10, 15);
-    nextInt = constrain(nextInt, 0, 15);
-    
-    decValue = (decValue * 16) + nextInt;
+void translateBT() {                // user written function, action based on BT signal received
+  while (BLU.available()) {         
+    delay(1);
+    BT += (char)BLU.read();
   }
-  
-  return decValue;
-}
 
+  if ( BT.length() <= 11 ) {       // check if string is in range of xxx/xxx/xxx 11 characters
+    startstring=0;
+    endstring = 1;
+    for ( var=0; var <= (int)BT.length() ; var++ ) {
+      if ( BT.charAt(var) == '/' ) {
+        if ( endstring == 1 ) {
+          Red_string = BT.substring(startstring, var);
+        }
+        else if ( endstring == 2 ) {
+          Green_string = BT.substring(startstring + 1, var);
+          Blue_string = BT.substring(var + 1, BT.length());
+        }
+        startstring = var;
+        endstring = endstring + 1;
+      }
+      else {
+        Red = 0;
+        Green = 0;
+        Blue = 0;
+        setrgb();
+      }
+    }
+
+  Red = Red_string.toInt();
+  Green = Green_string.toInt();
+  Blue = Blue_string.toInt();
+  setrgb();
+  BT="";
+  BLU.end();          // Flush serial communication
+  BLU.begin(9600);    // Restart a fresh serial
+  }
+  else {
+    Red = 0;
+    Green = 0;
+    Blue = 0;
+    setrgb();
+  }
+}
